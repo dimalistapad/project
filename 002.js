c32cd780358212a1ee50bfed3d6d7b34c5e2293c
Dima <dimalistapad@gmail.com>
2024-08-07 21:36:30 +0300

    second

---
+++ 002.js
@@ -0,0 +1,34 @@
+// 19. Условия
+
+"use strict"
+
+const num = 150;
+
+// if (4 == 1) {
+// 	console.log('Ok!');
+// }	else {
+// 	console.log('Error!');
+// }
+
+if (num < 49) {
+	console.log('Error');
+} else if (num > 100) {
+	console.log('Много');
+}	else {
+	console.log('Ok!');
+}
+
+// switch (num) {
+// 	case 49:
+// 		console.log('No');
+// 		break;
+// 	case 100:
+// 		console.log('No');
+// 		break;
+// 	case 50:
+// 		console.log('Yes');
+// 		break;
+// 	default:
+// 		console.log('So');
+// 		break;
+// }
